510d0b801520c45f7e4254a95345120185bf28f0
yq <yan_qiang_1@163.com>
2019-07-25 15:52:02 +0800

    深入理解删除节点

--- src/main/java/com/yq/leetcode/tree/BinaryTree.java
+++ src/main/java/com/yq/leetcode/tree/BinaryTree.java
@@ -167,10 +167,121 @@ public class BinaryTree {
      * 　　3、该节点有两个子节点
      */
     private boolean delete(int key){
+        //当前节点
+        Node current = root;
+        //当前节点的父节点
+        Node parent = root;
+        //判断当前节点是左节点还是右节点
+        boolean isLeftChild = false;
 
+        //这一部分主要是为了给 parent 和 current 赋值，找不到直接返回false
+        while(current.data != key){
+            //将当前值赋值给父节点，当做下次循环当前值得父节点
+            parent = current;
+            //判断左右节点
+            if(current.data > key){
+                isLeftChild = true;
+                //将左节点当做当前值，进行下次循环
+                current = current.leftChild;
+            }else{
+                isLeftChild = false;
+                //将右节点当做当前值，进行下次循环
+                current = current.rightChild;
+            }
+            //找不到返回false
+            if(current == null){
+                return false;
+            }
+        }
+
+        //如果当前节点没有子节点
+        if(current.leftChild == null && current.rightChild == null){
+            //如果是根节点，将根节点设置为 null，以便垃圾回收
+            if(current == root){
+                root = null;
+            }else if(isLeftChild){
+                //如果是左节点,将父节点的左节点，也就是当前节点设置为 null
+                parent.leftChild = null;
+            }else{
+                //如果不是左节点,将父节点的右节点，也就是当前节点设置为 null
+                parent.rightChild = null;
+            }
+            return true;
+
+        //当前节点只有一个右子节点,
+        //这段判断很有意思，可以想一下，为什么只判断一边就可以
+        }else if(current.leftChild == null && current.rightChild != null){
+            //如果当前节点只有一个右节点，并且当前节点还为根节点，那就把右子节点赋值给根节点
+            if(current == root){
+                root = current.rightChild;
+            }else if(isLeftChild){
+                //如果当前节点是父节点的左节点，就把当前节点的右节点覆盖给当前节点，也就是父节点的左节点
+                parent.leftChild = current.rightChild;
+            }else{
+                //如果当前节点是父节点的右节点，就把当前节点的右节点覆盖给当前节点，也就是父节点的左节点
+                parent.rightChild = current.rightChild;
+            }
+            return true;
+
+        //当前节点有一个左子节点，与上面同理
+        }else if(current.leftChild != null && current.rightChild == null){
+            //如果当前节点只有一个左子节点，并且当前节点还为根节点，那就把左子节点赋值给根节点
+            if(current == root){
+                root = current.leftChild;
+            }else if(isLeftChild){
+                //如果当前节点是父节点的左节点，就把当前节点的右节点覆盖给当前节点，也就是父节点的左节点
+                parent.leftChild = current.leftChild;
+            }else{
+                //如果当前节点是父节点的右节点，就把当前节点的右节点覆盖给当前节点，也就是父节点的左节点
+                parent.rightChild = current.leftChild;
+            }
+            return true;
+        }else{
+            //当前节点存在两个子节点
+            Node successor = getSuccessor(current);
+            //如果当前节点为根节点，那就把后继节点赋值给根节点
+            if(current == root){
+                root= successor;
+            }else if(isLeftChild){
+                //如果当前节点为左节点，那就把后继节点赋值给左节点
+                parent.leftChild = successor;
+            }else{
+                parent.rightChild = successor;
+            }
+            //最后把后继节点的左节点替换成将要删除节点的左节点
+            successor.leftChild = current.leftChild;
+        }
         return false;
     }
 
+    /**
+     * @Author : Yanqiang
+     * @Date : 2019-07-25
+     * @Params : [delNode]
+     * @Return : com.yq.leetcode.tree.BinaryTree.Node
+     * @Description : 获取后继节点
+     */
+    private Node getSuccessor(Node delNode){
+        //替换节点的父节点
+        Node successorParent = delNode;
+        //替换节点
+        Node successor = delNode;
+        //当前节点
+        Node current = delNode.rightChild;
+        //这个循环可以好好思考一下，很有意思的，跟上面的有点像，
+        //把替换节点当做下一个的父节点
+        while(current != null){
+            successorParent = successor;
+            successor = current;
+            current = current.leftChild;
+        }
+        //后继节点不是删除节点的右子节点，将后继节点替换删除节点
+        if(successor != delNode.rightChild){
+            successorParent.leftChild = successor.rightChild;
+            successor.rightChild = delNode.rightChild;
+        }
+        return successor;
+    }
 
     public static void main(String[] args) {
        /* BinaryTree binaryTree = new BinaryTree();
