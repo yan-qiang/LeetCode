36f4a599e3e3371475ffc645c23e5166ee436cb1
yq <yan_qiang_1@163.com>
2019-07-26 23:07:25 +0800

    初识插入排序

---
+++ src/main/java/com/yq/leetcode/Sort/InsertSort.java
@@ -0,0 +1,12 @@
+package com.yq.leetcode.Sort;
+
+/**
+ * @ClassName :  InsertSort
+ * @Author :  Yanqinag
+ * @Date :  2019-07-26 23:06
+ * @Description :  插入排序
+ */
+public class InsertSort {
+
+
+}
